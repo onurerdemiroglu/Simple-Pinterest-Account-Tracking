6f86ca52aadee1dfaab9e1edbcdfd9fdccce9fa4
onurerdemirrr <35347777+onurerdemirrr@users.noreply.github.com>
2021-10-22 16:25:28 +0300

    Simple Pinterest Account Tracking

---
+++ Pinterest_Tracker/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace PinterestToken
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ToplamHesapBan = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ToplamDomain = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toplamshadow = new System.Windows.Forms.TextBox();
+            this.Hesap = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.resimlink = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Hesap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1330, 644);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.Hesap);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1322, 611);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Pinterest Hesap Takip";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.ToplamHesapBan);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ToplamDomain);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.toplamshadow);
+            this.groupBox1.Location = new System.Drawing.Point(241, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1034, 66);
+            this.groupBox1.TabIndex = 30;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İstatistik";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(893, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "İşlem:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Toplam Hesap Ban:";
+            // 
+            // ToplamHesapBan
+            // 
+            this.ToplamHesapBan.Location = new System.Drawing.Point(164, 26);
+            this.ToplamHesapBan.Name = "ToplamHesapBan";
+            this.ToplamHesapBan.Size = new System.Drawing.Size(114, 26);
+            this.ToplamHesapBan.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(587, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Toplam Domain Ban:";
+            // 
+            // ToplamDomain
+            // 
+            this.ToplamDomain.Location = new System.Drawing.Point(750, 29);
+            this.ToplamDomain.Name = "ToplamDomain";
+            this.ToplamDomain.Size = new System.Drawing.Size(114, 26);
+            this.ToplamDomain.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(289, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Toplam Shadow Sayısı:";
+            // 
+            // toplamshadow
+            // 
+            this.toplamshadow.Location = new System.Drawing.Point(467, 26);
+            this.toplamshadow.Name = "toplamshadow";
+            this.toplamshadow.Size = new System.Drawing.Size(114, 26);
+            this.toplamshadow.TabIndex = 0;
+            // 
+            // Hesap
+            // 
+            this.Hesap.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Hesap.Location = new System.Drawing.Point(15, 69);
+            this.Hesap.Name = "Hesap";
+            this.Hesap.Size = new System.Drawing.Size(1301, 536);
+            this.Hesap.TabIndex = 29;
+            this.Hesap.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Hesap_CellContentDoubleClick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(29, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 48);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Hesap takip";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // resimlink
+            // 
+            this.resimlink.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.resimlink.FormattingEnabled = true;
+            this.resimlink.ItemHeight = 20;
+            this.resimlink.Location = new System.Drawing.Point(1983, 1013);
+            this.resimlink.Name = "resimlink";
+            this.resimlink.Size = new System.Drawing.Size(65, 64);
+            this.resimlink.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 663);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.resimlink);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pinterest Hesap Takip";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Hesap)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView Hesap;
+        private System.Windows.Forms.ListBox resimlink;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ToplamHesapBan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ToplamDomain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox toplamshadow;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Pinterest_Tracker/Form1.cs
@@ -0,0 +1,346 @@
+﻿using Newtonsoft.Json; 
+using System; 
+using System.Data;
+using System.Data.SqlClient; 
+using System.IO;
+using System.Linq;
+using System.Net; 
+using System.Text;
+using System.Threading; 
+using System.Windows.Forms; 
+
+namespace PinterestToken
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        string pinsayisi, takipcisayisi, shadowdurum, boardsayisi, goruntulenme,domaindurum;
+
+        private void Hesap_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            hesabagit();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //try
+            //{
+            //    th1.Abort(); 
+            //}
+            //catch (Exception)
+            //{ 
+            //}
+            ////browserkapat(); 
+        }
+        void hesabagit()
+        {
+            try
+            {
+                string target = "https://pinterest.com/" + Hesap.CurrentRow.Cells[1].Value.ToString();
+                System.Diagnostics.Process.Start(target);
+            }
+            catch
+            {
+            }
+        } 
+        void gridgetir()
+        {
+            try
+            {
+                con = new SqlConnection(@"server=Onurerdemiroglu; Initial Catalog=YAZILIM;Integrated Security=SSPI");
+                da = new SqlDataAdapter("SELECT ID,KullaniciAdi,Domain,Goruntulenme,PinSayisi,TakipciSayisi,BoardSayisi,DomainDurum,ShadowDurum,Guncelleme FROM Takip ORDER BY ID, KullaniciAdi, Domain, Goruntulenme, PinSayisi, TakipciSayisi, BoardSayisi, DomainDurum, ShadowDurum, Guncelleme ", con);
+                ds = new DataSet();
+                con.Open();
+                da.Fill(ds, "Takip");
+                Hesap.DataSource = ds.Tables["Takip"];
+                con.Close();
+
+                Hesap.Columns[0].Width = 50;
+                Hesap.Columns[1].Width = 120;
+                Hesap.Columns[2].Width = 120;
+                Hesap.Columns[3].Width = 120;
+                Hesap.Columns[4].Width = 120;
+                Hesap.Columns[5].Width = 120;
+                Hesap.Columns[6].Width = 120;
+                Hesap.Columns[7].Width = 120;
+                Hesap.Columns[8].Width = 120;
+                Hesap.Columns[9].Width = 180;
+            }
+            catch (Exception)
+            {
+            }
+        }
+        void gridgetir2()
+        {
+            try
+            {
+                con = new SqlConnection(@"server=Onurerdemiroglu; Initial Catalog=YAZILIM;Integrated Security=SSPI");
+                da = new SqlDataAdapter("SELECT name FROM sys.tables ORDER BY name ", con);
+                ds = new DataSet();
+                con.Open();
+                da.Fill(ds, "Takip");
+                Hesap.DataSource = ds.Tables["Takip"];
+                con.Close();
+                 
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        Thread th1;
+        int domainbansayi, hesapbansayi, shadowbansayi;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            th1 = new Thread(new ThreadStart(takip));
+            th1.Start();
+        } 
+        string mevcutgoruntulenme, mevcutpinsayisi, mevcuttakipcisayisi, mevcutboardsayisi, mevcutdomaindurum, mevcutshadowdurum;
+
+
+        void takip()
+        { 
+            domainbansayi = 0;
+            hesapbansayi = 0;
+            shadowbansayi=0;
+            for (int i = 0; i < Hesap.RowCount-1; i++)
+            {
+                try
+                {
+
+
+                    mevcutgoruntulenme = Hesap.Rows[i].Cells[3].Value.ToString().Split('(').First();
+                    mevcutpinsayisi = Hesap.Rows[i].Cells[4].Value.ToString().Split('(').First();
+                    mevcuttakipcisayisi = Hesap.Rows[i].Cells[5].Value.ToString().Split('(').First();
+                    mevcutboardsayisi = Hesap.Rows[i].Cells[6].Value.ToString().Split('(').First();
+                    mevcutdomaindurum = Hesap.Rows[i].Cells[7].Value.ToString().Split('(').First();
+                    mevcutshadowdurum = Hesap.Rows[i].Cells[8].Value.ToString().Split('(').First();
+
+                    DataGridViewRow limit2 = Hesap.Rows[i];
+                    limit2.Cells[3].Value = "······";
+                    limit2.Cells[4].Value = "······";
+                    limit2.Cells[5].Value = "······";
+                    limit2.Cells[6].Value = "······";
+                    limit2.Cells[7].Value = "······";
+                    limit2.Cells[8].Value = "······";
+                    limit2.Cells[9].Value = "······";
+
+                    string kullaniciadi = Hesap.Rows[i].Cells[1].Value.ToString().Replace(Environment.NewLine, "");
+                    string domain = Hesap.Rows[i].Cells[2].Value.ToString().Replace(Environment.NewLine, "").Replace("https://","");
+
+                    string hesapgetir = "https://tr.pinterest.com/resource/UserResource/get/?source_url=%2F" + kullaniciadi + "%2F&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22username%22%3A%22" + kullaniciadi + "%22%2C%22field_set_key%22%3A%22profile%22%7D%2C%22context%22%3A%7B%7D%7D&_=";
+                    string domaingetir = "https://tr.pinterest.com/resource/DomainFeedResource/get/?source_url=%2Fsource%2F" + domain + "&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22domain%22%3A%22" + domain + "%22%7D%2C%22context%22%3A%7B%7D%7D&_=";
+                    domaingetir = domaingetir.Replace(Environment.NewLine, "").Replace(" ", "");
+
+                    try
+                    {
+                        WebClient wc = new WebClient();
+                        Stream oku = wc.OpenRead(hesapgetir);
+                        StreamReader sr = new StreamReader(oku, Encoding.GetEncoding("windows-1254"));
+                        string gelen = sr.ReadToEnd();
+                        string HtmlKodum = gelen;
+                        dynamic array = JsonConvert.DeserializeObject(HtmlKodum);
+
+                        foreach (var item in array)
+                        {
+                            try
+                            {
+                                for (int b = 0; b < 100; b++)
+                                {
+                                    pinsayisi = array["resource_response"]["data"]["pin_count"].ToString();
+
+                                    //if (mevcutpinsayisi!="")
+                                    //{
+                                    //    if (Convert.ToInt32(pinsayisi) > Convert.ToInt32(mevcutpinsayisi))
+                                    //    {
+                                    //        pinsayisi = pinsayisi + "(+" + (Convert.ToInt32(pinsayisi)-Convert.ToInt32(mevcutpinsayisi)).ToString() + ")";
+                                    //    }
+                                    //    if (Convert.ToInt32(pinsayisi) < Convert.ToInt32(mevcutpinsayisi))
+                                    //    {
+                                    //        pinsayisi = pinsayisi + "(-" + (Convert.ToInt32(mevcutpinsayisi) - Convert.ToInt32(pinsayisi)).ToString() + ")";
+                                    //    }
+                                    //}
+
+
+                                    takipcisayisi = array["resource_response"]["data"]["follower_count"].ToString();
+
+                                    //if (mevcuttakipcisayisi != "")
+                                    //{
+                                    //    if (Convert.ToInt32(takipcisayisi) > Convert.ToInt32(mevcuttakipcisayisi))
+                                    //    {
+                                    //        takipcisayisi = takipcisayisi + "(+" + (Convert.ToInt32(takipcisayisi) - Convert.ToInt32(mevcuttakipcisayisi)).ToString() + ")";
+                                    //    }
+                                    //    if (Convert.ToInt32(takipcisayisi) < Convert.ToInt32(mevcuttakipcisayisi))
+                                    //    {
+                                    //        takipcisayisi = takipcisayisi + "(-" + (Convert.ToInt32(mevcuttakipcisayisi) - Convert.ToInt32(takipcisayisi)).ToString() + ")";
+                                    //    }
+                                    //}
+                                    boardsayisi = array["resource_response"]["data"]["board_count"].ToString();
+
+                                    //if (mevcutboardsayisi != "")
+                                    //{
+                                    //    if (Convert.ToInt32(boardsayisi) > Convert.ToInt32(mevcutboardsayisi))
+                                    //    {
+                                    //        boardsayisi = boardsayisi + "(+" + (Convert.ToInt32(boardsayisi) - Convert.ToInt32(mevcutboardsayisi)).ToString() + ")";
+                                    //    }
+                                    //    if (Convert.ToInt32(boardsayisi) < Convert.ToInt32(mevcutboardsayisi))
+                                    //    {
+                                    //        boardsayisi = boardsayisi + "(-" + (Convert.ToInt32(mevcutboardsayisi) - Convert.ToInt32(boardsayisi)).ToString() + ")";
+                                    //    }
+                                    //}
+
+                                    goruntulenme = array["resource_response"]["data"]["profile_reach"].ToString();
+
+                                    //if (mevcutgoruntulenme != "")
+                                    //{
+                                    //    if (Convert.ToInt32(goruntulenme) > Convert.ToInt32(mevcutgoruntulenme))
+                                    //    {
+                                    //        goruntulenme = goruntulenme + "(+" + (Convert.ToInt32(goruntulenme) - Convert.ToInt32(mevcutgoruntulenme)).ToString() + ")";
+                                    //    }
+                                    //    if (Convert.ToInt32(takipcisayisi) < Convert.ToInt32(mevcutgoruntulenme))
+                                    //    {
+                                    //        goruntulenme = goruntulenme + "(-" + (Convert.ToInt32(mevcutgoruntulenme) - Convert.ToInt32(goruntulenme)).ToString() + ")";
+                                    //    }
+                                    //} 
+
+                                    shadowdurum = array["resource_response"]["data"]["indexed"].ToString();
+
+                                    if (shadowdurum == "True")
+                                    {
+                                        shadowdurum = "Temiz";
+                                    }
+                                    if (shadowdurum == "False")
+                                    {
+                                        shadowdurum = "Shadowlu";
+                                        shadowbansayi += 1;
+                                    }
+
+                                    //if (mevcutshadowdurum != "")
+                                    //{
+                                    //    if (mevcutshadowdurum == "Temiz")
+                                    //    {
+                                    //        if (shadowdurum == "False")
+                                    //        {
+                                    //            shadowdurum = "Shadowlandı";
+                                    //        }
+                                    //    }
+                                    //}
+
+
+                                    if (goruntulenme == "-1")
+                                    {
+                                        goruntulenme = "0";
+                                    }
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                MessageBox.Show(e.ToString());
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        pinsayisi = "xxxxxxxxxxxx";
+                        goruntulenme = "xxxxxxxxxxxx";
+                        boardsayisi = "xxxxxxxxxxxx";
+                        takipcisayisi = "xxxxxxxxxxxx";
+                        shadowdurum = "xxxxxxxxxxxx";
+                        hesapbansayi += 1;
+                    }
+                   
+
+                    try
+                    {
+                        WebClient wc2 = new WebClient();
+                        Stream oku2 = wc2.OpenRead(domaingetir);
+                        StreamReader sr2 = new StreamReader(oku2, Encoding.GetEncoding("windows-1254"));
+                        string gelen2 = sr2.ReadToEnd();
+                        string HtmlKodum2 = gelen2;
+                        dynamic array2 = JsonConvert.DeserializeObject(HtmlKodum2);
+
+                        foreach (var item in array2)
+                        {
+                            try
+                            {
+                                domaindurum = array2["resource_response"]["data"][1]["board"]["image_thumbnail_url"].ToString();
+                                domaindurum = "Temiz";
+                            }
+                            catch (Exception)
+                            {
+                                domaindurum = "Banlı";
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        if (takipcisayisi== "xxxxxxxxxxxx")
+                        {
+                            domaindurum = "xxxxxxxxxxxx"; 
+                        }
+                        else
+                        {
+                            domaindurum = "Belirsiz"; 
+                        }
+                    }
+
+                    if (domaindurum=="Banlı")
+                    {
+                        domainbansayi += 1; 
+                    }
+
+                    DataGridViewRow limit = Hesap.Rows[i];
+                    limit.Cells[3].Value = goruntulenme;
+                    limit.Cells[4].Value = pinsayisi;
+                    limit.Cells[5].Value = takipcisayisi;
+                    limit.Cells[6].Value = boardsayisi;
+                    limit.Cells[7].Value = domaindurum;
+                    limit.Cells[8].Value = shadowdurum;
+                    limit.Cells[9].Value = DateTime.Now.ToString();
+                }
+                catch (Exception)
+                { 
+                }
+                string ID = Hesap.Rows[i].Cells[0].Value.ToString();
+                string KullaniciAdi = Hesap.Rows[i].Cells[1].Value.ToString();
+                string Domain = Hesap.Rows[i].Cells[2].Value.ToString();
+                string Görüntülenme = Hesap.Rows[i].Cells[3].Value.ToString();
+                string PinSayısı = Hesap.Rows[i].Cells[4].Value.ToString();
+                string TakipçiSayısı = Hesap.Rows[i].Cells[5].Value.ToString();
+                string BoardSayısı = Hesap.Rows[i].Cells[6].Value.ToString();
+                string DomainnDurum = Hesap.Rows[i].Cells[7].Value.ToString();
+                string ShadowDurum = Hesap.Rows[i].Cells[8].Value.ToString();
+                string Güncelleme = Hesap.Rows[i].Cells[9].Value.ToString();
+
+                cmd = new SqlCommand();
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "update Takip set KullaniciAdi='" + KullaniciAdi + "' " + ",Domain='" + Domain + "',Goruntulenme='" + Görüntülenme + "' ,PinSayisi='" + PinSayısı + "',TakipciSayisi='" + TakipçiSayısı + "',DomainDurum='" + DomainnDurum + "',BoardSayisi='" + BoardSayısı + "',ShadowDurum='" + ShadowDurum + "',Guncelleme='" + Güncelleme + "' where ID=" + ID + "";
+                cmd.ExecuteNonQuery();
+                con.Close();
+                label4.Text = "İşlem: " + (i+1).ToString();
+                ToplamHesapBan.Text = hesapbansayi.ToString();
+                toplamshadow.Text = shadowbansayi.ToString();
+                ToplamDomain.Text = domainbansayi.ToString();
+            }
+            
+        } 
+   
+         
+        private void Form1_Load_1(object sender, EventArgs e)
+        { 
+            gridgetir();   
+        }  
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlCommand cmd;
+        DataSet ds;
+       
+
+       
+
+    }
+}
